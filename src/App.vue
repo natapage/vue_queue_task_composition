cd57220aa41bb229f32bf462e9a134fa048cf6dc
natapage <624832@gmail.com>
2023-04-27 03:58:08 +0300

    composition version

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<script setup>
+import checkoutQueue from "./components/CheckoutQueue.vue";
+import { ref, onMounted, computed } from "vue";
+
+const queues = ref([[2, 3, 5], [2, 6, 3], [1], [11, 3, 6], []]);
+let newPurchasesQuantity = ref("");
+let isCorrectQuantity = ref(true);
+
+function addCustomer() {
+  newPurchasesQuantity.value = Number(newPurchasesQuantity.value);
+  if (
+    newPurchasesQuantity.value > 0 &&
+    Number.isInteger(newPurchasesQuantity.value)
+  ) {
+    isCorrectQuantity.value = true;
+    queues.value[shortestQueueIndex.value].push(newPurchasesQuantity.value);
+    newPurchasesQuantity.value = "";
+    return;
+  }
+  isCorrectQuantity.value = false;
+  newPurchasesQuantity.value = "";
+}
+
+function updateQueues() {
+  queues.value.forEach((queue) => {
+    if (queue.length === 0) {
+      return;
+    }
+    if (queue[0] === 1) {
+      queue.shift();
+      return;
+    }
+    queue[0] -= 1;
+  });
+}
+
+const shortestQueueIndex = computed(() => {
+  return queues.value
+    .map((array, index) => {
+      return array.reduce((acc, item) => acc + item, 0);
+    })
+    .reduce((acc, item, index, arr) => (item < arr[acc] ? index : acc), 0);
+});
+
+onMounted(() => setInterval(updateQueues, 2000));
+</script>
+
+<template>
+  <div id="app">
+    <h2 style="color: red" v-if="!isCorrectQuantity">
+      please enter a positive integer
+    </h2>
+    <div class="input-form">
+      <input
+        v-model="newPurchasesQuantity"
+        type="number"
+        class="input"
+        placeholder="Add purchases quantity.."
+      />
+      <button class="btn" @click="addCustomer">Create customer!</button>
+    </div>
+
+    <div class="checkout-zone">
+      <checkoutQueue
+        v-for="(checkoutQueue, index) in queues"
+        :key="index + 1"
+        :checkoutNumber="index + 1"
+        :queue="queues[index]"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.checkout-zone {
+  display: flex;
+}
+</style>
---
+++ src/components/CheckoutQueue.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="checkout-queue">
+    <div class="checkout">{{ checkoutNumber }}</div>
+    <div
+      class="customer"
+      v-for="(purchasesQuantity, index) in queue"
+      :key="index"
+    >
+      {{ purchasesQuantity }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    queue: {
+      type: Array,
+    },
+    checkoutNumber: {
+      type: Number,
+    },
+  },
+};
+</script>
+
+<style>
+.checkout-queue {
+  display: inline-block;
+  margin-top: 20px;
+  padding: 10px;
+}
+
+.checkout {
+  width: 60px;
+  height: 60px;
+  border-radius: 10px;
+  border: 2px solid gray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.customer {
+  width: 60px;
+  height: 60px;
+  border-radius: 50px;
+  border: 2px solid gray;
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
