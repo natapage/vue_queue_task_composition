ab8279ba6e793f5b7c08261a0821fa97c23cc44d
natapage <624832@gmail.com>
2023-04-28 03:35:19 +0300

    deleted id app

--- src/App.vue
+++ src/App.vue
@@ -46,33 +46,31 @@ onMounted(() => setInterval(updateQueues, 2000));
 </script>
 
 <template>
-  <div id="app">
-    <h2 style="color: red" v-if="!isCorrectQuantity">
-      please enter a positive integer
-    </h2>
-    <div class="input-form">
-      <input
-        v-model="newPurchasesQuantity"
-        type="number"
-        class="input"
-        placeholder="Add purchases quantity.."
-      />
-      <button class="btn" @click="addCustomer">Create customer!</button>
-    </div>
+  <h2 style="color: red" v-if="!isCorrectQuantity">
+    please enter a positive integer
+  </h2>
+  <div class="input-form">
+    <input
+      v-model="newPurchasesQuantity"
+      type="number"
+      class="input"
+      placeholder="Add purchases quantity.."
+    />
+    <button class="btn" @click="addCustomer">Create customer!</button>
+  </div>
 
-    <div class="checkout-zone">
-      <checkoutQueue
-        v-for="(checkoutQueue, index) in queues"
-        :key="index + 1"
-        :checkoutNumber="index + 1"
-        :queue="queues[index]"
-      ></checkoutQueue>
-    </div>
+  <div class="checkout-zone">
+    <checkoutQueue
+      v-for="(checkoutQueue, index) in queues"
+      :key="index + 1"
+      :checkoutNumber="index + 1"
+      :queue="queues[index]"
+    ></checkoutQueue>
   </div>
 </template>
 
 <style scoped>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
